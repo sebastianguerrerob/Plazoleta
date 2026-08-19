d64e32b92844f08e03af0c95648f5cd17dcb509f
sebastianguerrerob <jguerrerobautista4@gmail.com>
2026-08-19 14:31:06 -0500

    Clase Restaurante

---
+++ src/main/java/com/example/Plazoleta/domain/model/Restaurante.java
@@ -0,0 +1,21 @@
+package com.example.Plazoleta.domain.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Restaurante {
+    private Long id;
+    private String nombre;
+    private String direccion;
+    private int id_propietario;
+    private String telefono;
+    private String urlLogo;
+    private String nit;
+}
