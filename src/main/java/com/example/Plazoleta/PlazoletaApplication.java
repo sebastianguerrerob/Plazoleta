58be814ee61a4ac499cb4fd55688e12b396bb0e4
sebastianguerrerob <jguerrerobautista4@gmail.com>
2026-08-19 14:29:39 -0500

    Commit inicial

---
+++ src/main/java/com/example/Plazoleta/PlazoletaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Plazoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlazoletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlazoletaApplication.class, args);
+	}
+
+}
