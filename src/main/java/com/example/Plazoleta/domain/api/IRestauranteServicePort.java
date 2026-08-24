b0f9f99d2a74a3d6a945181916ed2e4994ff5cb7
sebastianguerrerob <jguerrerobautista4@gmail.com>
2026-08-24 11:56:30 -0500

    feat(restaurante): Add domain layer for listing restaurants with generic pagination

--- src/main/java/com/example/Plazoleta/domain/api/IRestauranteServicePort.java
+++ src/main/java/com/example/Plazoleta/domain/api/IRestauranteServicePort.java
@@ -1,7 +1,10 @@
 package com.example.Plazoleta.domain.api;
 
+import com.example.Plazoleta.domain.model.PaginatedResult;
+import com.example.Plazoleta.domain.model.PaginationRequest;
 import com.example.Plazoleta.domain.model.Restaurante;
 
 public interface IRestauranteServicePort {
     void crearRestaurante(Restaurante restaurante, String token);
+    PaginatedResult<Restaurante> listarRestaurantes(PaginationRequest paginationRequest);
 }
--- src/main/java/com/example/Plazoleta/domain/spi/IRestaurantePersistencePort.java
+++ src/main/java/com/example/Plazoleta/domain/spi/IRestaurantePersistencePort.java
@@ -1,5 +1,7 @@
 package com.example.Plazoleta.domain.spi;
 
+import com.example.Plazoleta.domain.model.PaginatedResult;
+import com.example.Plazoleta.domain.model.PaginationRequest;
 import com.example.Plazoleta.domain.model.Restaurante;
 
 import java.util.Optional;
@@ -7,4 +9,5 @@ import java.util.Optional;
 public interface IRestaurantePersistencePort {
     void guardarRestaurante(Restaurante restaurante);
     Optional<Restaurante> obtenerRestaurantePorId(Long id);
+    PaginatedResult<Restaurante> listarRestaurantesOrdenadosPorNombre(PaginationRequest paginationRequest);
 }
